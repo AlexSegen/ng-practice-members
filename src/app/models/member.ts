8f90e22340a4a044c4b745791fd2071fee8b9641
Alejandro Vivas <ajvivas86@gmail.com>
2019-12-08 12:56:29 -0300

    Update Member Model

--- src/app/models/member.ts
+++ src/app/models/member.ts
@@ -1,20 +1,7 @@
 export class Member {
 
     id:Number;
-    name:String;
-    email:String;
-    company:String;
-    salary:Number;
-    isActive:Boolean;
-    role:String;
-
-    constructor(name?:String, email?:String, company?:String, salary?:Number, role?:String, active?:Boolean) {
+    constructor(private name?:String, private email?:String, private company?:String, private salary?:Number, private role?:String, private isActive?:Boolean) {
         this.id = 0;
-        this.name = name;
-        this.email = email;
-        this.company = company;
-        this.salary = salary;
-        this.role = role;
-        this.isActive = active;
     }
 }
